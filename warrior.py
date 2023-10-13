6594be895b683138de29b4d37e30d269de296e5a
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-10-13 15:50:04 -0400

    using super on child classes, built attack method functionality

--- warrior.py
+++ warrior.py
@@ -9,6 +9,15 @@ class Warrior:
     def alive(self):
         return self.health > 0
     
+    # print status - an instance method
+    def print_status(self, other):
+        if other.alive():
+            print (f"\n{self.name} health: {self.health}")
+        if self.alive():
+            print (f"\n{other.name} health: {other.health}")
+        else:
+            pass
+    
 # hero class constructor (child of warrior)
 class Hero(Warrior):
     def __init__(self, name, health, power, attributes, weapon):
@@ -25,8 +34,15 @@ class Hero(Warrior):
         print(f"\nMy heroics know no bounds.  Villains fear me, other heroes wanna be me. - {self.name}")
 
     # hero attack
-    def attack(self):
-        print(f"\n{self} executes an attack")
+    def attack(self, other):
+        # print("test")
+        # print(f"{other.name}")
+        other.health -= self.power
+
+        if self.alive():
+            print(f"\n{self.name} executes an attack.  {other.name} takes damage")
+        else:
+            print(f"\n{self.name} has died.  Game is over, go home.  Life sucks.")
 
 # villain class constructor (child of warrior)
 class Villain(Warrior):
@@ -44,8 +60,8 @@ class Villain(Warrior):
         print(f"\nI shall corrupt the world!  Noone shall stop me! - {self.name}")
 
     # villain attack
-    def attack(self):
-        print(f"\n{self.name} plays dirty and executes an attack")
+    def attack(self, other):
+        print(f"\n{self.name} plays dirty and executes an attack.  It strikes {other.name}")
 
 # instances of hero class
 thor = Hero("Thor", 30, 15, "Asgardian God of Thudner", "Mjolnir")
@@ -58,50 +74,45 @@ zombie = Villain("The Mysterious Undead", float('inf'), 2, None, None)
 vader = Villain("Darth Vader", 60, 25, "Sith Lord", "Force Choke")
 
 # select a hero
-def hero_select():
-    print()
-    print("Select a hero")
-    print("1. Thor")
-    print("2. Sam")
-    print("3. Frodo Baggins")
-    print("> ",)
-    user_input = input()
-    if user_input == "1":
-        myhero = thor
-    elif user_input == "2":
-        myhero = sam
-    elif user_input == "3":
-        myhero = frodo
-    else:
-        print("\nInvalid input %r" % user_input)
-    myhero.greet()
-    myhero.announce()
-    return myhero
+print()
+print("Select a hero")
+print("1. Thor")
+print("2. Sam")
+print("3. Frodo Baggins")
+print("> ",)
+user_input = input()
+if user_input == "1":
+    myhero = thor
+elif user_input == "2":
+    myhero = sam
+elif user_input == "3":
+    myhero = frodo
+else:
+    print("\nInvalid input %r" % user_input)
+myhero.greet()
+myhero.announce()
 
-# select a villain
-def villain_select():
-    print()
-    print("Select a villain")
-    print("1. JavaScript")
-    print("2. The Mysterious Undead")
-    print("3. Darth Vader")
-    print("> ",)
-    user_input = input()
-    if user_input == "1":
-        myvillain = javascript
-    elif user_input == "2":
-        myvillain = zombie
-    elif user_input == "3":
-        myvillain = vader
-    else:
-        print("\nInvalid input %r" % user_input)
-    myvillain.greet()
-    myvillain.announce()
-    return myvillain
+print()
+print("Select a villain")
+print("1. JavaScript")
+print("2. The Mysterious Undead")
+print("3. Darth Vader")
+print("> ",)
+user_input = input()
+if user_input == "1":
+    myvillain = javascript
+elif user_input == "2":
+    myvillain = zombie
+elif user_input == "3":
+    myvillain = vader
+else:
+    print("\nInvalid input %r" % user_input)
+myvillain.greet()
+myvillain.announce()
 
-hero_select()
 
-villain_select()
+while myhero.alive() and myvillain.alive():
+    myhero.attack(myvillain)
 
 
 
